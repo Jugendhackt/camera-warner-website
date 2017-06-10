5f6a1ce3a869527dd4657fe7def2f569c797b517
Peter Mader <peter.mader@web.de>
2017-06-10 21:11:46 +0200

    add new camera icon

--- js/map.js
+++ js/map.js
@@ -8,13 +8,21 @@ var mapUpdateTimeOut;
 var markerList = [];
 var markerLayer;
 
+var cameraIcon = L.icon({
+    iconUrl: 'kamera_icon.png',
+
+    iconSize:     [50,48], // size of the icon
+    iconAnchor:   [25,25], // point of the icon which will correspond to marker's location
+    popupAnchor:  [15,15] // point from which the popup should open relative to the iconAnchor
+});
+
 function processJSON(data){
 
   var e;
   markerList = [];
   for(var i = 0; i<data.elements.length;i++) {
     e = data.elements[i];
-    markerList.push(L.marker([e.lat,e.lon]));
+    markerList.push(L.marker([e.lat,e.lon],{icon: cameraIcon}));
   }
   // prevent adding markers multiple times
   if (markerLayer != undefined){
