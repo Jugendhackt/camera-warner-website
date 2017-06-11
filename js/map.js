d3f03bbededb13ddf940db784cd459db2cb3ee7c
Peter Mader <peter.mader@web.de>
2017-06-11 10:02:47 +0200

    add jhhh-2017 location

--- js/map.js
+++ js/map.js
@@ -23,6 +23,16 @@ var camera_warningIcon = L.icon({
     popupAnchor:  [25,25] // point from which the popup should open relative to the iconAnchor
 });
 
+var alpakaIcon = L.icon({
+    iconUrl: 'icons/alpaka.png',
+
+    iconSize:     [70,70], // size of the icon
+    iconAnchor:   [35,42], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0,-30] // point from which the popup should open relative to the iconAnchor
+});
+
+var alpaka = L.marker([53.56249,9.95990],{icon: alpakaIcon}).addTo(mymap);
+alpaka.bindPopup("Jugend hackt Hamburg 2017")
 function processJSON(data){
 
   var e;
